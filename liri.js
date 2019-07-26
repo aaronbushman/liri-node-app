4394c1159d3fa895b801c6321a0afa245c3d1db8
aaron <aaronbushman@gmail.com>
2019-07-26 00:29:51 -0400

    adding mp4 of app

--- liri.js
+++ liri.js
@@ -51,7 +51,7 @@ function doThis() {
 function concertThis(value) {
     axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             for (var i = 0; i < response.data.length; i++) {
 
                 var datetime = response.data[i].datetime;
